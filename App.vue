047c7965cdb7f8d0529b8d03a4bde0f40b6cfb13
JOHAN MOGOLLON JIMENEZ <144180781+johanmogollonjime@users.noreply.github.com>
2024-12-10 19:43:52 -0500

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,15 @@
+<template>
+  <ion-app>
+    <router-view></router-view> <!-- Aquí se cargarán las vistas según la ruta -->
+  </ion-app>
+</template>
+
+<script>
+export default {
+  name: 'App'
+};
+</script>
+
+<style>
+/* Aquí puedes agregar los estilos generales de tu aplicación */
+</style>
